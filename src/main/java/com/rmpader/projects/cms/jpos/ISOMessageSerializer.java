1bdad078978985f5420f2c70be1e1afeab7c89e2
Reynald Pader <reynaldmpader@gmail.com>
2014-05-08 15:55:17 +0800

    adjusted ISO serializers

--- src/main/java/com/rmpader/projects/cms/jpos/ISOMessageSerializer.java
+++ src/main/java/com/rmpader/projects/cms/jpos/ISOMessageSerializer.java
@@ -4,20 +4,44 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
+
+import javax.xml.bind.DatatypeConverter;
 
 import org.jpos.iso.ISOMsg;
+import org.jpos.iso.ISOPackager;
+import org.jpos.iso.ISOUtil;
 import org.springframework.core.serializer.Deserializer;
 import org.springframework.core.serializer.Serializer;
 
 public class ISOMessageSerializer implements Deserializer<ISOMsg>,
 		Serializer<ISOMsg> {
 
+	public static final int DEFAULT_LENGTH_HEADER_SIZE = 4;
+
+	private final int lengthHeaderSize;
+	private final ISOPackager packager;
+
+	public ISOMessageSerializer(ISOPackager packager) {
+		this(DEFAULT_LENGTH_HEADER_SIZE, packager);
+	}
+
+	public ISOMessageSerializer(int size, ISOPackager packager) {
+		this.lengthHeaderSize = size;
+		this.packager = packager;
+	}
+
 	@Override
 	public void serialize(ISOMsg object, OutputStream outputStream)
 			throws IOException {
 		try {
+			object.setPackager(packager);
 			byte[] bytes = object.pack();
-			BufferedOutputStream stream = new BufferedOutputStream(outputStream,bytes.length);
+			byte[] headerBytes =ISOUtil.zeropad(Integer.toString(bytes.length), lengthHeaderSize).getBytes();
+			BufferedOutputStream stream = new BufferedOutputStream(
+					outputStream, bytes.length + lengthHeaderSize);
+			stream.write(ByteBuffer.allocate(lengthHeaderSize).put(headerBytes)
+					.array());
 			stream.write(bytes);
 			stream.flush();
 		} catch (Exception e) {
@@ -27,8 +51,30 @@ public class ISOMessageSerializer implements Deserializer<ISOMsg>,
 
 	@Override
 	public ISOMsg deserialize(InputStream inputStream) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			byte[] headerBytes = new byte[lengthHeaderSize];
+			int readBytes = inputStream.read(headerBytes);
+			if (readBytes == lengthHeaderSize) {
+				int messageLength = Integer.valueOf(DatatypeConverter
+						.printHexBinary(headerBytes));
+				byte[] bytes = new byte[messageLength];
+				inputStream.read(bytes);
+				ISOMsg msg = packager.createISOMsg();
+				msg.setPackager(packager);
+				msg.unpack(bytes);
+				return msg;
+			} else {
+				// DO something
+				return null;
+			}
+		} catch (Exception e) {
+			throw new IOException("Failed to deserialize ISO Message", e);
+		}
+
+	}
+
+	public int getLengthHeaderSize() {
+		return lengthHeaderSize;
 	}
 
 }
--- src/test/java/com/rmpader/projects/cms/jpos/ISOMessageSerializerTest.java
+++ src/test/java/com/rmpader/projects/cms/jpos/ISOMessageSerializerTest.java
@@ -2,8 +2,6 @@ package com.rmpader.projects.cms.jpos;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.IOException;
-
 import javax.xml.bind.DatatypeConverter;
 
 import org.jpos.iso.ISOException;
@@ -11,29 +9,55 @@ import org.jpos.iso.ISOMsg;
 import org.jpos.iso.packager.GenericPackager;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.core.serializer.Deserializer;
-import org.springframework.core.serializer.Serializer;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 
-@SuppressWarnings({ "rawtypes", "unchecked" })
 public class ISOMessageSerializerTest {
 
-	private Serializer serializer = new ISOMessageSerializer();
-	private Deserializer deserializer = new ISOMessageSerializer();
+	private ISOMessageSerializer serializer;
+	private ISOMessageSerializer deserializer;
+	private String hexString;
+	private ISOMsg msg;
 
-	@Test
-	public void testSerialize() throws ISOException, IOException {
-		ISOMsg msg = new ISOMsg("0800");
+	@Before
+	public void setup() throws ISOException {
+		serializer = new ISOMessageSerializer(new GenericPackager(this
+				.getClass().getResource("packager.xml").getPath()));
+		deserializer = serializer;
+		hexString = "00400800202000000080000000000000000129110001";
+
+		msg = new ISOMsg("0800");
 		msg.set(3, "000000");
 		msg.set(11, "000001");
 		msg.set(41, "29110001");
-		
-		msg.setPackager(new GenericPackager(this.getClass().getResource("packager.xml").getPath()));
-		
-		ByteOutputStream stream = new ByteOutputStream(msg.pack().length);
+	}
+
+	@Test
+	public void testSerialize() throws Exception {
+		ByteOutputStream stream = new ByteOutputStream(
+				DatatypeConverter.parseHexBinary(hexString).length);
 		serializer.serialize(msg, stream);
 
-		assertEquals("0800202000000080000000000000000129110001", new String(stream.getBytes()));
+		assertEquals(hexString, new String(stream.getBytes()));
 	}
+
+	@Test
+	public void testDeserialize() throws Exception {
+		byte[] inputBytes = hexString.getBytes();
+		ByteInputStream stream = new ByteInputStream(inputBytes,
+				inputBytes.length);
+
+		ISOMsg actual = (ISOMsg) deserializer.deserialize(stream);
+
+		assertEqualISO(msg, actual);
+
+	}
+	
+	private void assertEqualISO(ISOMsg expected, ISOMsg actual) throws ISOException{
+		for(int i = 0; i <= 128; i++){
+			assertEquals(expected.getValue(i),actual.getValue(i));
+		}
+	}
+
 }
